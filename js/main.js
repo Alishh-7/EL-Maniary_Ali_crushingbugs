b994508079224a98dab16942938c7f8db1854b9c
Alishh-7 <156357009+Alishh-7@users.noreply.github.com>
2024-06-30 01:44:29 -0400

    update the file
    in this commit, the base of the files was updated due to my mistakes.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ console.log("Javascript is connected");
 //variables
 const theButtons = document.querySelectorAll("#buttonHolder img");
 const puzzleBoard = document.querySelector(".puzzle-board");
+const puzzlePieces = document.querySelectorAll(".puzzle-pieces img");
+const dropZones = document.querySelectorAll(".drop-zone");
+let draggedPiece;
 
 //functions
 function changeBGImage(event) {
@@ -17,5 +20,25 @@ function changeBGImage(event) {
     puzzleBoard.style.backgroundImage = `url('./images/backGround${event.currentTarget.id}.jpg')`;
 }
 
+function handleStartDrag() {
+    console.log(`started dragging ${this}`);
+    draggedPiece = this;
+}
+
+function handleOver(e) {
+    e.preventDefault();
+    console.log("Dragged Over")
+}
+
+function handleDrop() {
+    this.appendChild(draggedPiece);
+}
+
 //eventListeners
-theButtons.forEach(button => button.addEventListener("click", changeBGImage))
+theButtons.forEach(button => button.addEventListener("click", changeBGImage));
+
+puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
+
+dropZones.forEach(zone => zone.addEventListener("dragover", handleOver));
+
+dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
